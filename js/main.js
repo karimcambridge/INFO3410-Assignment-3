5c61d2fc1cf1a59e592c226ac16a9c5b87e4733f
zariagrant <37633897+zariagrant@users.noreply.github.com>
2018-03-29 19:22:18 -0400

    Minor changes
    Co-Authored-By: Kar <karimcambridge@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 if (typeof global !== "undefined" && typeof require !== "undefined"){ // to accomodate difference between node and the browser
-   // const fs = require('fs');
+    const fs = require('fs');
     const AnimeManager = require('animeManger').AnimeManager;
 
     function insertTblData(){
