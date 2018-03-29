aa4671d10efc2de4e589a8e451ae9ab2b0f4c560
Kar <karimcambridge@gmail.com>
2018-03-29 19:39:03 -0400

    Fix main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-if (typeof global !== "undefined" && typeof require !== "undefined"){ // to accomodate difference between node and the browser
-    const fs = require('fs');
-    const AnimeManager = require('animeManger').AnimeManager;
-=======
+
 /* jshint esversion: 6 */
->>>>>>> bdbec1c66fca10eb878e2bc713105d6c568357a9
 
 console.log(typeof global);
 
