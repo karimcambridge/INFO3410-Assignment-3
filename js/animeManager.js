ce8e37801c06058399136ddb135cd92a591356bd
AnnaBreton <32850138+AnnaBreton@users.noreply.github.com>
2018-03-30 15:43:24 -0400

    comment update

--- js/animeManager.js
+++ js/animeManager.js
@@ -1,7 +1,7 @@
 /* jshint esversion: 6 */
 
 if(typeof global !== "undefined" && typeof require !== "undefined") { // to accomodate difference between node and the browser
-    var fs = require('browserify-fs');
+    let fs = require('fs');
     const Anime = require('../js/anime').Anime;
 
     class AnimeManager {
@@ -12,14 +12,14 @@ if(typeof global !== "undefined" && typeof require !== "undefined") { // to acco
         // reads records from anime.csv file and store each record as an instance of the anime class
         // Each instance will be stored in the records attribute of the animeManager object
         readFile() {
-            const raw_data = fs.readFileSync("../assets/anime.csv");
-            const raw_records_row = raw_data.split("\n");
+            let raw_data = fs.readFileSync("assets/anime.csv") + '';
+            let raw_records_row = raw_data.split("\n");
 
-            for(let i = 1; i < raw_record_rows.length; i+= 1){
-                const rec = raw_record_rows[i];
-                const comp = rec.split(",");
+            for(let i = 1; i < raw_records_row.length; i++) {
+                let rec = raw_records_row[i] + '';
+                let comp = rec.split(",");
 
-                if(comp.length > 5){
+                if(comp.length > 5) {
                     let p = new Anime(comp[0], comp[1], comp[2], comp[3], comp[4], comp[5], comp[6]);
                     this.records.push(p);
                 } //end if
@@ -28,9 +28,6 @@ if(typeof global !== "undefined" && typeof require !== "undefined") { // to acco
 
         //used the toHTMLRow method of each instance to create the table HTML string
         generateTable() {
-            if(this.records.length < 1)
-                this.readFile();
-            
             let htmlStr = "<table>";
             htmlStr += "<thead><tr><th>ID</th><th>Name</th>";
             htmlStr += "<th>Genre</th><th>Type</th><th>Episodes</th>";
@@ -47,6 +44,7 @@ if(typeof global !== "undefined" && typeof require !== "undefined") { // to acco
         //Uses the generateTable method and save the code generated to the file "table.html"
         writeHTML() {
             let htmlStr = this.generateTable();
+            console.log(htmlStr);
             fs.writeFileSync("table.html", htmlStr);
         }
     }
