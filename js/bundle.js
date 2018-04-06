b86899b86451385159da05dd240e804aba95a83d
Kar <karimcambridge@gmail.com>
2018-04-06 15:02:45 -0400

    Convert spacing to tabs and menu update

--- js/bundle.js
+++ js/bundle.js
@@ -3,7 +3,7 @@ require=(function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=
 /* jshint esversion: 6 */
 
 function myFunction(x) {
-    alert("Row index is: " + x.rowIndex);
+	alert("Row index is: " + x.rowIndex);
 }
 
 class Anime {
@@ -17,42 +17,42 @@ class Anime {
 		this.members = members;
 	}
 
-    toHTMLRow() {
-        let currentRating = 0;
-        //Modal Testing. Can customize function
-        let str = "<tr onclick='function hi(x){alert(x.cells[2].innerHTML)}; hi(this)'>";
-        //let str = "<tr onclick=alert(this.innerText)>";
-        //let str = "<tr onclick='myFunction(this)'>";
-        console.log("onclick test " + str);
+	toHTMLRow() {
+		let currentRating = 0;
+		//Modal Testing. Can customize function
+		let str = "<tr onclick='function hi(x){alert(x.cells[2].innerHTML)}; hi(this)'>";
+		//let str = "<tr onclick=alert(this.innerText)>";
+		//let str = "<tr onclick='myFunction(this)'>";
+		console.log("onclick test " + str);
 
-        //iterating the keys of an object
+		//iterating the keys of an object
 
-        for(let key in this){
-            str += "<td>"+ this[key] +"</td>"; //access the value of that key in the object
-            if(key == "rating"){
-                currentRating = parseInt(this[key]);
-            }
-            //console.log(key);
-        }
-        if(currentRating >= 7 ) {
-            str += "<td><img src='happyface.png' alt='' border=3 height=20 width=20></img> </td>";
-        } else if (currentRating >= 5){
-            str += "<td><img src='neutralface.png' alt='' border=3 height=20 width=20></img> </td>";
-        } else if (currentRating >= 1){
-            str += "<td><img src='frownface.png' alt='' border=3 height=20 width=20></img> </td>";
-        } else {
-            str += "<td><img src='thinkingface.png' alt='' border=3 height=20 width=20></img> </td>";
-        }
+		for(let key in this){
+			str += "<td>"+ this[key] +"</td>"; //access the value of that key in the object
+			if(key == "rating"){
+				currentRating = parseInt(this[key]);
+			}
+			//console.log(key);
+		}
+		if(currentRating >= 7 ) {
+			str += "<td><img src='happyface.png' alt='' border=3 height=20 width=20></img> </td>";
+		} else if (currentRating >= 5){
+			str += "<td><img src='neutralface.png' alt='' border=3 height=20 width=20></img> </td>";
+		} else if (currentRating >= 1){
+			str += "<td><img src='frownface.png' alt='' border=3 height=20 width=20></img> </td>";
+		} else {
+			str += "<td><img src='thinkingface.png' alt='' border=3 height=20 width=20></img> </td>";
+		}
 
-        str += "</tr>";
-        return str;
-    }
+		str += "</tr>";
+		return str;
+	}
 }
 
 if(global && module && module.exports) {
-    module.exports = {
-        'Anime': Anime
-    };
+	module.exports = {
+		'Anime': Anime
+	};
 }
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],2:[function(require,module,exports){
