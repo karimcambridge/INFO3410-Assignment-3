342a4069ff64cd6aa3f23b5e7130620c4420efba
zariagrant <37633897+zariagrant@users.noreply.github.com>
2018-04-10 14:09:19 -0400

    install rest_framework

--- animeManager/animeMan/programmes/urls.py
+++ animeManager/animeMan/programmes/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
+from django.conf.urls import url, include 
+from . import views
 from .views import list_programmes, add_programmes, RecordView
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('', list_programmes),
     path('add', add_programmes),
     path('add.html', add_programmes),
-    path('records', RecordView.as_view())
+    path('records', RecordView.as_view()),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/', include(router.urls))
 ]
