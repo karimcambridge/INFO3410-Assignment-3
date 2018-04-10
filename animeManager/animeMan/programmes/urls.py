e796efa16f9591562a01d5d05a8087e4d2282522
zariagrant <37633897+zariagrant@users.noreply.github.com>
2018-04-10 17:34:45 -0400

    minor updates

--- animeManager/animeMan/programmes/urls.py
+++ animeManager/animeMan/programmes/urls.py
@@ -6,6 +6,7 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
+router.register(r'signup', views.SignUpViewSet)
 
 urlpatterns = [
     path('', list_programmes),
