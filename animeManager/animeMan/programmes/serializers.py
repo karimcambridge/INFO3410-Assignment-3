dd70dc8477ab46b7143326182931caa837cb3358
zariagrant <37633897+zariagrant@users.noreply.github.com>
2018-04-10 17:21:26 -0400

    trying auth. user ...again

--- animeManager/animeMan/programmes/serializers.py
+++ animeManager/animeMan/programmes/serializers.py
@@ -4,4 +4,19 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email')
+        fields = ('url', 'username', 'email', 'password')
+
+class SignUpSerializer(serializers.Serializer):
+    username = serializers.CharField(
+        max_length = 100,
+        style = {'placeholder': 'Username'}
+    )
+    email = serializers.EmailField(
+        max_length = 100,
+        style = {'placeholder': 'Email', 'autofocus': True}
+    )
+    password = serializers.CharField(
+        max_length = 100,
+        style = {'input_type': 'password', 'placeholder': 'Password'}
+    )
+    remember_me = serializers.BooleanField()
